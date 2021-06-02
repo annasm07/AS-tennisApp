ceb4bdd74ccb2495c216642e56770d7f0c905f12
Anna Sala Mercade <anna@Annas-MacBook-Pro.local>
2021-06-02 16:22:19 +0200

    Add global themes stylesheet file

---
+++ AS-FinalProject/frontend/src/theme/globalThemes.tsx
@@ -0,0 +1,34 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  body: {
+    marginTop: 50,
+    marginHorizontal: 20,
+  },
+  buttonYellow: {
+    alignItems: 'center',
+    alignSelf: 'center',
+    backgroundColor: '#F5BF00',
+    borderRadius: 20,
+    borderColor: '#000000',
+    borderWidth: 1,
+    fontSize: 50,
+    marginTop: 30,
+    padding: 12,
+    width: 120,
+  },
+  buttonGray: {
+    alignItems: 'center',
+    alignSelf: 'center',
+    backgroundColor: '#EFEFEF',
+    borderRadius: 20,
+    borderColor: '#000000',
+    borderWidth: 1,
+    fontSize: 50,
+    marginTop: 10,
+    padding: 12,
+    width: 100,
+  },
+});
+
+export default styles;
