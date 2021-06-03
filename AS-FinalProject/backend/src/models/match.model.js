f1927136149991a585c329818a7c94118faeb8b7
Anna Sala Mercade <anna@Annas-MacBook-Pro.local>
2021-06-03 10:14:59 +0200

    Update/Refresh match model

--- AS-FinalProject/backend/src/models/match.model.js
+++ AS-FinalProject/backend/src/models/match.model.js
@@ -1,23 +1,14 @@
 const mongoose = require('mongoose');
 
 const matchSchema = mongoose.Schema({
-  player1: {
-    type: mongoose.Types.ObjectId,
-    ref: 'Players',
-  },
-  player2: {
-    type: mongoose.Types.ObjectId,
-    ref: 'Players',
-  },
-  result: {
-    firstSet: [Number],
-    secondSet: [Number],
-    thirdSet: [Number],
-  },
-  winner: Boolean,
+  result: [{
+    player: [mongoose.Types.ObjectId],
+    games: [Number],
+  }],
+  winner: [mongoose.Types.ObjectId],
   date: Date,
-  stats: [String],
-  duration: String,
+  stats: [mongoose.Types.ObjectId],
+  duration: Date,
 });
 
 module.exports = mongoose.model('Matches', matchSchema);
