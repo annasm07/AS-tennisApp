0a557e3ab40ab219123f47da40681db7ce658d74
Anna Sala Mercade <anna@Annas-MacBook-Pro.local>
2021-06-03 19:00:41 +0200

    Add token Reducer to index reducer

--- AS-FinalProject/frontend/src/redux/reducers/index.js
+++ AS-FinalProject/frontend/src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import player from './playerReducer';
+import tokens from './tokenReducer';
 
 const rootReducer = combineReducers({
   player,
+  tokens,
 });
 
 export default rootReducer;
