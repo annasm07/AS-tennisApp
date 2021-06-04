2f29343b529952225b8fbcf89937958381e70ac4
Anna Sala Mercade <anna@Annas-MacBook-Pro.local>
2021-06-04 12:50:26 +0200

    Dashboard component working

--- AS-FinalProject/frontend/src/components/Dashboard/Dashboard.tsx
+++ AS-FinalProject/frontend/src/components/Dashboard/Dashboard.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import {Text} from 'react-native';
+import * as React from 'react';
+import {View, Image} from 'react-native';
 
-const Dashboard = () => {
-  return <Text>Dashboard working! </Text>;
-};
-
-export default Dashboard;
+export default function Dashboard() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Image source={require('../../images/home-icon.png')} />
+    </View>
+  );
+}
