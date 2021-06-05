fdb0ee7e0deb00870904f238378230501ad1d3c8
Anna Sala Mercade <anna@Annas-MacBook-Pro.local>
2021-06-05 17:51:00 +0200

    Create New Match Component

---
+++ AS-FinalProject/frontend/src/components/NewMatch/NewMatch.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import {Text} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+
+export default function NewMatch() {
+  return (
+    <SafeAreaView>
+      <Text>New Match LIST PAGE IS WORKING</Text>
+    </SafeAreaView>
+  );
+}
