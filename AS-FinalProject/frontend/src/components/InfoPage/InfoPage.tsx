9cf80e8169dd05bdec640d0da5f9e74579cbba48
Anna Sala Mercade <anna@Annas-MacBook-Pro.local>
2021-06-05 17:51:19 +0200

    Create Info Page Component

---
+++ AS-FinalProject/frontend/src/components/InfoPage/InfoPage.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import {Text} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+
+export default function InfoPage() {
+  return (
+    <SafeAreaView>
+      <Text>Info Page LIST PAGE IS WORKING</Text>
+    </SafeAreaView>
+  );
+}
