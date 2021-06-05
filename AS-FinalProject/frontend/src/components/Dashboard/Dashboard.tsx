4b0a24b32b21e18093c376d4e8b178cf474cb293
Anna Sala Mercade <anna@Annas-MacBook-Pro.local>
2021-06-05 17:58:57 +0200

    Print Played Matches list of logged player

--- AS-FinalProject/frontend/src/components/Dashboard/Dashboard.tsx
+++ AS-FinalProject/frontend/src/components/Dashboard/Dashboard.tsx
@@ -7,6 +7,7 @@ function Dashboard({dispatch, player, tokens, user}: any) {
   useEffect(() => {
     dispatch(getPlayerInfo(tokens[0], user.user.playerId));
   }, [dispatch, tokens, user]);
+  console.log(player);
   return (
     <SafeAreaView>
       <View style={styles.playerBox}>
@@ -17,7 +18,35 @@ function Dashboard({dispatch, player, tokens, user}: any) {
           }}
         />
         <Text style={styles.playerName}>{player.name}</Text>
+        <View>
+          {player.record && (
+            <Text>
+              {' '}
+              {player.record[0]} / {player.record[1]}
+            </Text>
+          )}
+        </View>
       </View>
+
+      {player.playedMatches &&
+        player.playedMatches.map((match: any) => (
+          <>
+            <View style={styles.matchBox}>
+              <View style={styles.player1}>
+                <Text>{match.result[0].name}</Text>
+                <Text>{match.result[0].games}</Text>
+              </View>
+              <View style={styles.player2}>
+                <Text>{match.result[1].name}</Text>
+                <Text>{match.result[1].games}</Text>
+              </View>
+            </View>
+            <View style={styles.dateBox}>
+              <Text>MAY 23rd, 2021</Text>
+              <Text>Stats &gt;</Text>
+            </View>
+          </>
+        ))}
     </SafeAreaView>
   );
 }
@@ -38,6 +67,35 @@ const styles = StyleSheet.create({
   playerName: {
     fontSize: 20,
     paddingLeft: 20,
+  },
+  matchBox: {
+    backgroundColor: '#E5E5E5',
+    borderRadius: 10,
+    padding: 10,
+    margin: 15,
+    marginBottom: 5,
+  },
+  dateBox: {
+    backgroundColor: '#E5E5E5',
+    borderRadius: 10,
+    padding: 8,
+    marginHorizontal: 15,
+    marginBottom: 15,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    color: '#7A7A7A',
+  },
+  player1: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    paddingVertical: 8,
+  },
+  player2: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 8,
   },
 });
 
