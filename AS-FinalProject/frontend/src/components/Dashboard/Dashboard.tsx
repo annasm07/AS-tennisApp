f4b1b365f2a7497560a04a5f095d3f18d6a88141
Anna Sala Mercade <anna@Annas-MacBook-Pro.local>
2021-06-05 15:33:34 +0200

    Update Dashboard to print player data

--- AS-FinalProject/frontend/src/components/Dashboard/Dashboard.tsx
+++ AS-FinalProject/frontend/src/components/Dashboard/Dashboard.tsx
@@ -1,10 +1,52 @@
-import * as React from 'react';
-import {View, Image} from 'react-native';
+import React, {useEffect} from 'react';
+import {SafeAreaView, View, Text, Image, StyleSheet} from 'react-native';
+import {connect} from 'react-redux';
+import {getPlayerInfo} from '../../redux/actions/actionCreators';
 
-export default function Dashboard() {
+function Dashboard({dispatch, player, tokens, user}: any) {
+  useEffect(() => {
+    dispatch(getPlayerInfo(tokens[0], user.user.playerId));
+  }, [dispatch, tokens, user]);
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Image source={require('../../images/home-icon.png')} />
-    </View>
+    <SafeAreaView>
+      <View style={styles.playerBox}>
+        <Image
+          style={styles.playerImage}
+          source={{
+            uri: `${player.img}`,
+          }}
+        />
+        <Text style={styles.playerName}>{player.name}</Text>
+      </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  playerBox: {
+    backgroundColor: '#E5E5E5',
+    borderRadius: 10,
+    padding: 10,
+    margin: 15,
+    flexDirection: 'row',
+  },
+  playerImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 100,
+  },
+  playerName: {
+    fontSize: 20,
+    paddingLeft: 20,
+  },
+});
+
+function mapStateToProps({player, tokens, user}: any) {
+  return {
+    player,
+    tokens,
+    user,
+  };
+}
+
+export default connect(mapStateToProps)(Dashboard);
