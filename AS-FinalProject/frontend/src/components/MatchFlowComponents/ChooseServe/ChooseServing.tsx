90d3c84b13764552eeda2751ab14e74ed275253a
Anna Sala Mercade <anna@Annas-MacBook-Pro.local>
2021-06-07 11:29:42 +0200

    Choose serving page created

---
+++ AS-FinalProject/frontend/src/components/MatchFlowComponents/ChooseServe/ChooseServing.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import {Text} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+
+export default function Stats() {
+  return (
+    <SafeAreaView>
+      <Text>Choose serving PAGE IS WORKING</Text>
+    </SafeAreaView>
+  );
+}
