323a3d8a4b28ab1fb5415cf4fb7341103d7943dd
Anna Sala Mercade <anna@Annas-MacBook-Pro.local>
2021-06-07 12:38:13 +0200

    Create Serving component

---
+++ AS-FinalProject/frontend/src/components/MatchFlowComponents/Serving/Serving.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import {Text} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+
+export default function Stats() {
+  return (
+    <SafeAreaView>
+      <Text>SERVING PAGE PAGE IS WORKING</Text>
+    </SafeAreaView>
+  );
+}
