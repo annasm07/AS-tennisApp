faeb5f877ebbf084b842782e34459f00088a0887
Anna Sala Mercade <anna@Annas-MacBook-Pro.local>
2021-06-07 17:58:36 +0200

    Choose serving done with mocked values

--- AS-FinalProject/frontend/src/components/MatchFlowComponents/ChooseServe/ChooseServing.tsx
+++ AS-FinalProject/frontend/src/components/MatchFlowComponents/ChooseServe/ChooseServing.tsx
@@ -1,11 +1,31 @@
 import * as React from 'react';
-import {Text} from 'react-native';
-import {SafeAreaView} from 'react-native-safe-area-context';
+import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
+import matchBoxStyles from '../../../theme/matchBoxTheme';
 
-export default function Stats() {
+export default function Stats({navigation}: any) {
   return (
-    <SafeAreaView>
-      <Text>Choose serving PAGE IS WORKING</Text>
-    </SafeAreaView>
+    <View style={styles.view}>
+      <Text style={styles.title}>Who is Serving?</Text>
+      <TouchableOpacity
+        style={matchBoxStyles.matchBox}
+        onPress={() => navigation.navigate('PointFlow')}>
+        <Text>PLAYER 1 NAME</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={matchBoxStyles.matchBox}
+        onPress={() => navigation.navigate('PointFlow')}>
+        <Text>PLAYER 2 NAME</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  view: {
+    height: 100,
+  },
+  title: {
+    fontSize: 26,
+    paddingLeft: 20,
+  },
+});
