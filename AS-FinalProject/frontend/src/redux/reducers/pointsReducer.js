516c3f5a9bfc87f6b2f6fb8c3958637f15960993
annasm07 <anna.sala.mercade@gmail.com>
2021-06-10 09:54:18 +0200

    Create pointssReducer

---
+++ AS-FinalProject/frontend/src/redux/reducers/pointsReducer.js
@@ -0,0 +1,21 @@
+import actionTypes from '../actions/actionTypes';
+
+function currentGamePointsReducer(
+  currentGamePoints = {
+    points: [{p1: 0, p2: 0}],
+    p1CounterPoints: 0,
+    p2CounterPoints: 0,
+  },
+  action,
+) {
+  switch (action.type) {
+    case actionTypes.UPDATE_POINTS:
+      return action.currentGame;
+    case actionTypes.UPDATE_POINTS_ERROR:
+      return currentGamePoints;
+    default:
+      return currentGamePoints;
+  }
+}
+
+export default currentGamePointsReducer;
