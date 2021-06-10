cf0092448df3b5cfcf7f379520c946e365c7400c
annasm07 <anna.sala.mercade@gmail.com>
2021-06-10 09:53:10 +0200

    REfactor match model move flow out of results array of objects

--- AS-FinalProject/backend/src/models/match.model.js
+++ AS-FinalProject/backend/src/models/match.model.js
@@ -4,14 +4,14 @@ const matchSchema = mongoose.Schema({
   result: [{
     player: [mongoose.Types.ObjectId],
     name: String,
-    flow: {
-      points: [[{ p1: String, p2: String }]],
-      games: [{ p1: String, p2: String }],
-      sets: [{ p1: String, p2: String }],
-    },
     games: [Number],
     stats: mongoose.Types.ObjectId,
   }],
+  flow: {
+    points: [[{ p1: String, p2: String }]],
+    games: [{ p1: String, p2: String }],
+    sets: [{ p1: String, p2: String }],
+  },
   winner: [mongoose.Types.ObjectId],
   date: Date,
   duration: Date,
