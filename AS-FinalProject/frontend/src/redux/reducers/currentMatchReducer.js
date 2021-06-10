c29f34fbce73d89534faa01d7f0fb7ecc909ce6a
annasm07 <anna.sala.mercade@gmail.com>
2021-06-10 09:57:53 +0200

    Delete console logs from currentMatch Reducer

--- AS-FinalProject/frontend/src/redux/reducers/currentMatchReducer.js
+++ AS-FinalProject/frontend/src/redux/reducers/currentMatchReducer.js
@@ -1,8 +1,6 @@
 import actionTypes from '../actions/actionTypes';
 
 function currentMatchReducer(currentMatch = {}, action) {
-  console.log('reducer');
-  console.log(action.currentMatch);
   switch (action.type) {
     case actionTypes.CREATE_MATCH:
       return action.currentMatch;
