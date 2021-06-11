7cb59138aff6c1043ee0318d5670f636326b6735
annasm07 <anna.sala.mercade@gmail.com>
2021-06-11 19:23:30 +0200

    Add END_SET case for setsREducer

--- AS-FinalProject/frontend/src/redux/reducers/setsReducer.js
+++ AS-FinalProject/frontend/src/redux/reducers/setsReducer.js
@@ -1,7 +1,12 @@
 import actionTypes from '../actions/actionTypes';
+import {counterLogicScoring} from '../../utils/counterLogic';
 
 function currentGamePointsReducer(sets = [{p1: 0, p2: 0}], action) {
   switch (action.type) {
+    case actionTypes.END_SET:
+      const updatedSets = counterLogicScoring(action.player, sets);
+      console.log('updatedSets --->', updatedSets);
+      return updatedSets;
     case actionTypes.END_MATCH:
       return [{p1: 0, p2: 0}];
     default:
