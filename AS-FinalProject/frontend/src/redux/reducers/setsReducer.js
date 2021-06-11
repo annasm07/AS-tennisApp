460f1c8160b3e1cb79a027865c6f0eac28280014
annasm07 <anna.sala.mercade@gmail.com>
2021-06-11 12:12:24 +0200

    Create SetsREducer

---
+++ AS-FinalProject/frontend/src/redux/reducers/setsReducer.js
@@ -0,0 +1,12 @@
+import actionTypes from '../actions/actionTypes';
+
+function currentGamePointsReducer(sets = [{p1: 0, p2: 0}], action) {
+  switch (action.type) {
+    case actionTypes.END_MATCH:
+      return [{p1: 0, p2: 0}];
+    default:
+      return sets;
+  }
+}
+
+export default currentGamePointsReducer;
