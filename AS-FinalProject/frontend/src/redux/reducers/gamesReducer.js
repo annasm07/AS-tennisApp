744353ecf7a127d965b5634047b39162540906c3
annasm07 <anna.sala.mercade@gmail.com>
2021-06-11 19:42:02 +0200

    Add update games case to gamesReducer

--- AS-FinalProject/frontend/src/redux/reducers/gamesReducer.js
+++ AS-FinalProject/frontend/src/redux/reducers/gamesReducer.js
@@ -2,7 +2,7 @@ import actionTypes from '../actions/actionTypes';
 
 function currentGamePointsReducer(games = [{p1: 0, p2: 0}], action) {
   switch (action.type) {
-    case actionTypes.END_SET:
+    case actionTypes.END_SET || actionTypes.UPDATE_GAMES:
       return [{p1: 0, p2: 0}];
     default:
       return games;
