eddeb343f137ce70c00dba6b9450ef78e3560607
annasm07 <anna.sala.mercade@gmail.com>
2021-06-12 22:09:33 +0200

    Refactor current match  reducer

--- AS-FinalProject/frontend/src/redux/reducers/currentMatchReducer.js
+++ AS-FinalProject/frontend/src/redux/reducers/currentMatchReducer.js
@@ -6,6 +6,25 @@ function currentMatchReducer(currentMatch = {}, action) {
       return action.currentMatch;
     case actionTypes.CREATE_MATCH_ERROR:
       return currentMatch;
+    case actionTypes.UPDATE_MATCH_GAMES:
+      const gamesP1 = action.games[action.games.length - 1].p1;
+      const gamesP2 = action.games[action.games.length - 1].p2;
+      const updatedMatchGames = currentMatch;
+      updatedMatchGames.result[0].games[currentMatch.flow.sets.length || 0] =
+        gamesP1;
+      updatedMatchGames.result[1].games[currentMatch.flow.sets.length || 0] =
+        gamesP2;
+      updatedMatchGames.flow.points.push(action.points);
+      return updatedMatchGames;
+    case actionTypes.UPDATE_MATCH_SETS:
+      const updatedMatchSets = currentMatch;
+      updatedMatchSets.flow.games.push(action.games);
+      updatedMatchSets.flow.sets.push(action.sets);
+
+      updatedMatchSets.flow.points.push(action.points);
+      return updatedMatchSets;
+    // case actionTypes.UPDATE_MATCH:
+    //   return action.currentMatch;
     default:
       return currentMatch;
   }
