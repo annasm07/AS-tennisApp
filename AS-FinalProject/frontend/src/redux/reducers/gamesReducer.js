bdbaddf7a44a610b139491982e3d7c96bafdfb1e
annasm07 <anna.sala.mercade@gmail.com>
2021-06-12 09:13:07 +0200

    Refactor games and points reducers

--- AS-FinalProject/frontend/src/redux/reducers/gamesReducer.js
+++ AS-FinalProject/frontend/src/redux/reducers/gamesReducer.js
@@ -2,7 +2,9 @@ import actionTypes from '../actions/actionTypes';
 
 function currentGamePointsReducer(games = [{p1: 0, p2: 0}], action) {
   switch (action.type) {
-    case actionTypes.END_SET || actionTypes.UPDATE_GAMES:
+    case actionTypes.UPDATE_GAMES:
+      return [{p1: 0, p2: 0}];
+    case actionTypes.END_SET:
       return [{p1: 0, p2: 0}];
     default:
       return games;
