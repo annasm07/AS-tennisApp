62782c8ebf0f84056dcff8c09d9410669a1998e9
annasm07 <anna.sala.mercade@gmail.com>
2021-06-12 22:08:42 +0200

    Refactor Points reducer

--- AS-FinalProject/frontend/src/redux/reducers/pointsReducer.js
+++ AS-FinalProject/frontend/src/redux/reducers/pointsReducer.js
@@ -1,4 +1,5 @@
 import actionTypes from '../actions/actionTypes';
+import {counterLogicPoints} from '../../utils/counterLogic';
 
 function currentGamePointsReducer(
   currentGamePoints = {
@@ -10,16 +11,23 @@ function currentGamePointsReducer(
 ) {
   switch (action.type) {
     case actionTypes.UPDATE_POINTS:
-      return action.currentGame;
+      const updatedPoints = counterLogicPoints(
+        action.playerWhoWon,
+        currentGamePoints,
+      );
+      return updatedPoints;
+
     case actionTypes.UPDATE_POINTS_ERROR:
       return currentGamePoints;
-    case actionTypes.END_GAME:
+
+    case actionTypes.UPDATE_GAMES:
       return {
         points: [{p1: 0, p2: 0}],
         p1CounterPoints: 0,
         p2CounterPoints: 0,
       };
-    case actionTypes.END_SET:
+
+    case actionTypes.UPDATE_SETS:
       return {
         points: [{p1: 0, p2: 0}],
         p1CounterPoints: 0,
