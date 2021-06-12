f06372badf39b0d2757feabc4798369809727e98
annasm07 <anna.sala.mercade@gmail.com>
2021-06-12 22:10:46 +0200

    Refactor Check End Match

--- AS-FinalProject/frontend/src/utils/checkEndSet.js
+++ AS-FinalProject/frontend/src/utils/checkEndSet.js
@@ -17,17 +17,9 @@ export function checkEndSet(playerWhoWon, games) {
 
 export function checkEndMatch(playerWhoWon, sets) {
   let set = false;
-  const gamesScore = sets[sets.length - 1];
-  let playerWhoLost;
-  playerWhoWon === 'p1' ? (playerWhoLost = 'p2') : (playerWhoLost = 'p1');
-  if (
-    gamesScore[playerWhoWon] >= 6 &&
-    gamesScore[playerWhoWon] - gamesScore[playerWhoLost] >= 2
-  ) {
-    set = true;
-  } else if (gamesScore[playerWhoWon] === 7 && gamesScore[playerWhoLost] <= 6) {
-    set = true;
-  }
+  const setsScore = sets[sets.length - 1];
+
+  setsScore[playerWhoWon] === 2 ? (set = true) : (set = false);
 
   return set;
 }
