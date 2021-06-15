342101a6978eeaaae9804f24e2f6c7b96e0621fc
annasm07 <anna.sala.mercade@gmail.com>
2021-06-15 09:32:01 +0200

    Add scroll view and format date function to Dashboard

--- AS-FinalProject/frontend/src/components/Dashboard/Dashboard.tsx
+++ AS-FinalProject/frontend/src/components/Dashboard/Dashboard.tsx
@@ -6,6 +6,7 @@ import {
   Image,
   StyleSheet,
   TouchableOpacity,
+  ScrollView,
 } from 'react-native';
 import {connect} from 'react-redux';
 import {getPlayerInfo} from '../../redux/actions/actionCreators';
@@ -16,90 +17,99 @@ function Dashboard({dispatch, player, tokens, user, navigation}: any) {
   useEffect(() => {
     dispatch(getPlayerInfo(tokens[0], user.user.playerId));
   }, [dispatch, tokens, user]);
+
+  function getDateString(dateString: any) {
+    const dateToDisplay = new Date(dateString).toLocaleDateString();
+    return dateToDisplay;
+  }
   return (
     <SafeAreaView>
-      <View style={styles.playerBox}>
-        <Image
-          style={styles.playerImage}
-          source={{
-            uri: `${player.img}`,
-          }}
-        />
-        <View style={styles.infoBox}>
-          <Text style={styles.playerName}>{player.name}</Text>
+      <ScrollView>
+        <View style={styles.playerBox}>
+          <Image
+            style={styles.playerImage}
+            source={{
+              uri: `${player.img}`,
+            }}
+          />
+          <View style={styles.infoBox}>
+            <Text style={styles.playerName}>{player.name}</Text>
 
-          <View>
-            {player.record && (
-              <View style={styles.statsFullBar}>
-                <View
-                  style={[
-                    styles.statsWins,
-                    {
-                      width: `${
-                        (player.record[0] /
-                          (player.record[0] + player.record[1])) *
-                        100
-                      }%`,
-                    },
-                  ]}
-                />
-                <View
-                  style={[
-                    styles.statsLosses,
-                    {
-                      width: `${
-                        (player.record[1] /
-                          (player.record[0] + player.record[1])) *
-                        100
-                      }%`,
-                    },
-                  ]}
-                />
-              </View>
-            )}
-            <View style={styles.recordInfo}>
-              <Text style={globalStyles.grayText}>Wins / Losses</Text>
+            <View>
               {player.record && (
-                <Text style={globalStyles.grayText}>
-                  {' '}
-                  {player.record[0]} / {player.record[1]}
-                </Text>
+                <View style={styles.statsFullBar}>
+                  <View
+                    style={[
+                      styles.statsWins,
+                      {
+                        width: `${
+                          (player.record[0] /
+                            (player.record[0] + player.record[1])) *
+                          100
+                        }%`,
+                      },
+                    ]}
+                  />
+                  <View
+                    style={[
+                      styles.statsLosses,
+                      {
+                        width: `${
+                          (player.record[1] /
+                            (player.record[0] + player.record[1])) *
+                          100
+                        }%`,
+                      },
+                    ]}
+                  />
+                </View>
               )}
+              <View style={styles.recordInfo}>
+                <Text style={globalStyles.grayText}>Wins / Losses</Text>
+                {player.record && (
+                  <Text style={globalStyles.grayText}>
+                    {' '}
+                    {player.record[0]} / {player.record[1]}
+                  </Text>
+                )}
+              </View>
             </View>
           </View>
         </View>
-      </View>
 
-      {player.playedMatches &&
-        player.playedMatches.map((match: any) => (
-          <TouchableOpacity
-            onPress={() => {
-              navigation.navigate('Stats', {
-                matchId: match._id,
-              });
-            }}>
-            <View key={match._id}>
-              <View style={matchBoxStyles.matchBox}>
-                <View style={matchBoxStyles.player1}>
-                  <Text>{match.result[0].name}</Text>
-                  <Text style={matchBoxStyles.result}>
-                    {match.result[0].games}
-                  </Text>
+        {player.playedMatches &&
+          player.playedMatches.map((match: any) => (
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate('Stats', {
+                  matchId: match._id,
+                });
+              }}>
+              <View key={match._id}>
+                <View style={matchBoxStyles.matchBox}>
+                  <View style={matchBoxStyles.player1}>
+                    <Text>{match.result[0].name}</Text>
+                    <Text style={matchBoxStyles.result}>
+                      {match.result[0].games}
+                    </Text>
+                  </View>
+                  <View style={matchBoxStyles.player2}>
+                    <Text>{match.result[1].name}</Text>
+                    <Text style={matchBoxStyles.result}>
+                      {match.result[1].games}
+                    </Text>
+                  </View>
                 </View>
-                <View style={matchBoxStyles.player2}>
-                  <Text>{match.result[1].name}</Text>
-                  <Text style={matchBoxStyles.result}>
-                    {match.result[1].games}
+                <View style={matchBoxStyles.dateBox}>
+                  <Text style={globalStyles.grayText}>
+                    {getDateString(match.date)}
                   </Text>
+                  <Text style={globalStyles.grayText}>Stats &gt;</Text>
                 </View>
               </View>
-              <View style={matchBoxStyles.dateBox}>
-                <Text style={globalStyles.grayText}>{match.date}</Text>
-                <Text style={globalStyles.grayText}>Stats &gt;</Text>
-              </View>
-            </View>
-          </TouchableOpacity>
-        ))}
+            </TouchableOpacity>
+          ))}
+      </ScrollView>
     </SafeAreaView>
   );
 }
