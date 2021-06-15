49529dc3caa840535136ea2e93c1353f993f2d36
annasm07 <anna.sala.mercade@gmail.com>
2021-06-15 13:06:53 +0200

    Update UsersList Component Filter to return all except current logged user add styles display name and image

--- AS-FinalProject/frontend/src/components/UsersList/UsersList.tsx
+++ AS-FinalProject/frontend/src/components/UsersList/UsersList.tsx
@@ -1,13 +1,18 @@
 import React, {useEffect} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import {getAllPlayers} from '../../redux/actions/actionCreators';
-import {Text, StyleSheet} from 'react-native';
+import {Text, StyleSheet, View, Image, ScrollView} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
 function UsersList() {
   let tokens = useSelector((store: any) => store.tokens);
   let players = useSelector((store: any) => store.players);
+  let player = useSelector((store: any) => store.player);
   const dispatch = useDispatch();
+
+  const newPlayersArray = players.filter(
+    (playerInList: any) => playerInList._id !== player._id,
+  );
 
   useEffect(() => {
     dispatch(getAllPlayers(tokens[0]));
@@ -15,19 +20,49 @@ function UsersList() {
 
   return (
     <SafeAreaView>
-      <Text style={styles.title}>Find Other Users</Text>
-      {players.length ? (
-        players.map((player: any) => (
-          <Text style={styles.names}>- {player.name}</Text>
-        ))
-      ) : (
-        <Text>...loading...</Text>
-      )}
+      <ScrollView style={styles.scroll}>
+        <Text style={styles.title}>Find Other Users</Text>
+        {players.length ? (
+          newPlayersArray.map((playerToPrint: any) => (
+            <View style={styles.playerBox}>
+              <Image
+                style={styles.playerImage}
+                source={{
+                  uri: `${playerToPrint.img}`,
+                }}
+              />
+              <View>
+                <Text style={styles.names} key={playerToPrint._id}>
+                  {playerToPrint.name}
+                </Text>
+              </View>
+            </View>
+          ))
+        ) : (
+          <Text>...loading...</Text>
+        )}
+      </ScrollView>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  scroll: {
+    paddingTop: 30,
+    marginBottom: 90,
+  },
+  playerBox: {
+    backgroundColor: '#E5E5E5',
+    borderRadius: 10,
+    padding: 10,
+    margin: 15,
+    flexDirection: 'row',
+  },
+  playerImage: {
+    width: 60,
+    height: 60,
+    borderRadius: 100,
+  },
   title: {
     fontSize: 26,
     paddingLeft: 20,
