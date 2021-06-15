05653716b0d83e5ce8b29efb36542e94ee90234f
annasm07 <anna.sala.mercade@gmail.com>
2021-06-15 18:43:11 +0200

    Test FixedHeader component - 100% coverage

---
+++ AS-FinalProject/frontend/src/components/FixedHeader/FixedHeader.test.js
@@ -0,0 +1,22 @@
+import {render} from '@testing-library/react-native';
+import React from 'react';
+import ConfigureStore from 'redux-mock-store';
+import {Provider} from 'react-redux';
+import FixedHeader from './FixedHeader';
+
+jest.mock('../../redux/actions/actionCreators');
+
+describe('When invoked a Dashboard component', () => {
+  const mockStore = ConfigureStore();
+  const store = mockStore({
+    user: {user: {playerId: '123cde'}},
+  });
+  test('Should render dashboard component', () => {
+    const fixedHeader = render(
+      <Provider store={store}>
+        <FixedHeader />
+      </Provider>,
+    );
+    expect(fixedHeader).toMatchSnapshot();
+  });
+});
