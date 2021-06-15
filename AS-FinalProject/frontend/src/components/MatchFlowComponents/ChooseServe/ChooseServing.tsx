bb43ab009e75dd0709bac0b866f62692ff58d8d4
annasm07 <anna.sala.mercade@gmail.com>
2021-06-15 09:35:21 +0200

    Refactor choose serving to go left or right depending on the chosen server

--- AS-FinalProject/frontend/src/components/MatchFlowComponents/ChooseServe/ChooseServing.tsx
+++ AS-FinalProject/frontend/src/components/MatchFlowComponents/ChooseServe/ChooseServing.tsx
@@ -1,21 +1,28 @@
 import * as React from 'react';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
 import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
 import matchBoxStyles from '../../../theme/matchBoxTheme';
+import {updateServer} from '../../../redux/actions/actionCreators';
 
 export default function Stats({navigation}: any) {
   const currentMatch = useSelector((store: any) => store.currentMatch);
+  const dispatch = useDispatch();
+
+  function handleClick(player: Boolean) {
+    dispatch(updateServer(player));
+    navigation.navigate('PointFlow');
+  }
   return currentMatch.result ? (
     <View style={styles.view}>
       <Text style={styles.title}>Who is Serving?</Text>
       <TouchableOpacity
         style={matchBoxStyles.matchBox}
-        onPress={() => navigation.navigate('PointFlow')}>
+        onPress={() => handleClick(true)}>
         <Text>{currentMatch?.result[0]?.name}</Text>
       </TouchableOpacity>
       <TouchableOpacity
         style={matchBoxStyles.matchBox}
-        onPress={() => navigation.navigate('PointFlow')}>
+        onPress={() => handleClick(false)}>
         <Text>{currentMatch?.result[1]?.name}</Text>
       </TouchableOpacity>
     </View>
