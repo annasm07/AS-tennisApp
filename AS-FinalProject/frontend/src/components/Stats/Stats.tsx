122885099490724f1ee73db4ed96425c1198a32a
annasm07 <anna.sala.mercade@gmail.com>
2021-07-16 16:46:12 +0200

    Printing points into Stats page- not done

--- AS-FinalProject/frontend/src/components/Stats/Stats.tsx
+++ AS-FinalProject/frontend/src/components/Stats/Stats.tsx
@@ -1,6 +1,13 @@
 import React, {useEffect} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
-import {Text, View, Image, StyleSheet, TouchableOpacity} from 'react-native';
+import {
+  Text,
+  View,
+  Image,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {
   getStatsInfo,
@@ -30,70 +37,79 @@ export default function Stats({route, navigation}: any) {
   return (
     <SafeAreaView>
       <Text style={styles.title}>Match Details</Text>
+      <ScrollView>
+        {match && (
+          <>
+            <View style={matchBoxStyles.matchBox}>
+              <View style={matchBoxStyles.player1}>
+                <Text>{match?.result[0].name}</Text>
+                <Text style={matchBoxStyles.result}>
+                  {match?.result[0].games}
+                </Text>
+              </View>
+              <View style={matchBoxStyles.player2}>
+                <Text>{match?.result[1].name}</Text>
+                <Text style={matchBoxStyles.result}>
+                  {match?.result[1].games}
+                </Text>
+              </View>
+            </View>
+            <View style={styles.names}>
+              <Text style={styles.names}>{match?.result[0].name}</Text>
+              <Text style={styles.names}>{match?.result[1].name}</Text>
+            </View>
+          </>
+        )}
 
-      {match && (
-        <>
-          <View style={matchBoxStyles.matchBox}>
-            <View style={matchBoxStyles.player1}>
-              <Text>{match?.result[0].name}</Text>
-              <Text style={matchBoxStyles.result}>
-                {match?.result[0].games}
-              </Text>
+        {stats?.length ? (
+          <>
+            <StatsBar
+              title={'Aces'}
+              p1Value={stats[0]?.serve.aces}
+              p2Value={stats[1]?.serve.aces}
+            />
+            <StatsBar
+              title={'Double Faults'}
+              p1Value={stats[0]?.serve.doubleFaults}
+              p2Value={stats[1]?.serve.doubleFaults}
+            />
+            <StatsBar
+              title={'Forced Errors'}
+              p1Value={stats[0]?.points.forcedErrors.baseLine[0]}
+              p2Value={stats[1]?.points.forcedErrors.baseLine[0]}
+            />
+            <StatsBar
+              title={'Unforced Errors'}
+              p1Value={stats[0]?.points.unforcedErrors.baseLine[0]}
+              p2Value={stats[1]?.points.unforcedErrors.baseLine[0]}
+            />
+            <StatsBar
+              title={'Winners'}
+              p1Value={stats[0]?.points.winners.baseLine[0]}
+              p2Value={stats[1]?.points.winners.baseLine[0]}
+            />
+            <TouchableOpacity
+              style={styles.deleteButton}
+              onPress={() => handleDelete()}>
+              <Text>Delete</Text>
+            </TouchableOpacity>
+          </>
+        ) : (
+          <Image
+            source={{
+              uri: 'https://www.gatoslechuzos.com/static/loading.gif',
+            }}
+            style={styles.loading}
+          />
+        )}
+        {match && (
+          <>
+            <View>
+              <Text>{match?.flow.points[0][3].p1}</Text>
             </View>
-            <View style={matchBoxStyles.player2}>
-              <Text>{match?.result[1].name}</Text>
-              <Text style={matchBoxStyles.result}>
-                {match?.result[1].games}
-              </Text>
-            </View>
-          </View>
-          <View style={styles.names}>
-            <Text style={styles.names}>{match?.result[0].name}</Text>
-            <Text style={styles.names}>{match?.result[1].name}</Text>
-          </View>
-        </>
-      )}
-      {stats?.length ? (
-        <>
-          <StatsBar
-            title={'Aces'}
-            p1Value={stats[0]?.serve.aces}
-            p2Value={stats[1]?.serve.aces}
-          />
-          <StatsBar
-            title={'Double Faults'}
-            p1Value={stats[0]?.serve.doubleFaults}
-            p2Value={stats[1]?.serve.doubleFaults}
-          />
-          <StatsBar
-            title={'Forced Errors'}
-            p1Value={stats[0]?.points.forcedErrors.baseLine[0]}
-            p2Value={stats[1]?.points.forcedErrors.baseLine[0]}
-          />
-          <StatsBar
-            title={'Unforced Errors'}
-            p1Value={stats[0]?.points.unforcedErrors.baseLine[0]}
-            p2Value={stats[1]?.points.unforcedErrors.baseLine[0]}
-          />
-          <StatsBar
-            title={'Winners'}
-            p1Value={stats[0]?.points.winners.baseLine[0]}
-            p2Value={stats[1]?.points.winners.baseLine[0]}
-          />
-          <TouchableOpacity
-            style={styles.deleteButton}
-            onPress={() => handleDelete()}>
-            <Text>Delete</Text>
-          </TouchableOpacity>
-        </>
-      ) : (
-        <Image
-          source={{
-            uri: 'https://www.gatoslechuzos.com/static/loading.gif',
-          }}
-          style={styles.loading}
-        />
-      )}
+          </>
+        )}
+      </ScrollView>
     </SafeAreaView>
   );
 }
