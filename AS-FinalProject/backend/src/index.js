cd026d90942eef00670e6d0645adf33d071e0e00
annasm07 <anna.sala.mercade@gmail.com>
2021-09-02 12:20:05 +0200

    Fixed backend route url
    npm i to frontend to install updates

--- AS-FinalProject/backend/src/index.js
+++ AS-FinalProject/backend/src/index.js
@@ -19,7 +19,7 @@ server.use(morgan('dev'));
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
 
-server.use('/api/auth/', authRoutes);
+server.use('/api/auth', authRoutes);
 
 server.use(
   '/api',
